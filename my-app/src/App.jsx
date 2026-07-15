dd3e003b750a8a4d2744e9089df893491ce7a7dd
yamurb <yagmurbaycol@gmail.com>
2026-07-15 17:59:09 +0300

    initial commit practice

---
+++ my-app/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from './Header';
+import Footer from './Footer';
+import Card from './card';
+function App() {
+ return(
+    <>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </>
+      
+    
+ )
+}
+
+export default App
+  
---
+++ my-app/src/footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p>© 2023 My website</p>
+        </footer>
+    )
+}
+ export default Footer
