4871f477441f712f8eed98923fe15fec04e63adc
yamurb <yagmurbaycol@gmail.com>
2026-07-16 01:52:03 +0300

    mapping and sort practice react

--- my-app/src/List.jsx
+++ my-app/src/List.jsx
@@ -1,9 +1,21 @@
 function List(){
 
-    const fruits=["Apple","Banana","Mango","Orange","Grapes"];
+    const fruits=[ { id:1, name:"apple", calories: 95 },
+        { id:2, name:"banana", calories: 105 },
+        { id:3, name:"mango", calories: 200 },
+        { id:4, name:"orange", calories: 130 },
+        { id:5, name:"grapes", calories: 150 }];
 
-    const listItems=fruits.map(fruit => <li>{fruit}</li>);
+        //sorting the fruits array based on calories in ascending order
+        fruits.sort((a,b)=> a.calories - b.calories);//numeric  order
+        fruits.sort((a,b) => b.calories - a.calories);//reverse numeric order
+        fruits.sort((a,b) => a.name.localeCompare(b.name));//alphabetical order
+        fruits.sort((a,b) => b.name.localeCompare(a.name));//reverse alphabetical order
 
-    return <ul>{listItems}</ul>;
+    const listItems=fruits.map(fruit => <li key={fruit.id}>
+        {fruit.name}:&nbsp;
+        {fruit.calories} calories</li>);
+
+    return <ol><b>{listItems}</b></ol>;
 }
 export default List
